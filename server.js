f3333e5fbecc2ac2e9fee0d3bb585360ee58680d
felipesagredo <felipesagredogalindo@gmail.com>
2023-09-29 18:27:47 -0300

    Realizando deploying y Actualizacion

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 // Configuración de CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
