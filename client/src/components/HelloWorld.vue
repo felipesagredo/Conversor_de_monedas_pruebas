ecf5c91b32224c48c244a1ba2b9fad38df6383b8
felipesagredo <felipesagredogalindo@gmail.com>
2023-09-29 13:23:28 -0300

    Agregando fecha a la solicitud de la api

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1>Valor Actual de la UF en CLP</h1>
+    <h1>Valor de la UF en CLP para una fecha específica</h1>
+    <input type="date" v-model="selectedDate">
     <button @click="getUFValue">Obtener Valor de la UF</button>
-    <p v-if="ufValue">{{ ufValue }}</p>
+    <p v-if="ufValue">Fecha: {{ formattedDate(ufValue.fecha) }}, Valor: ${{ ufValue.valor }} CLP</p>
   </div>
 </template>
 
@@ -12,22 +13,38 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      ufValue: null
+      ufValue: null,
+      selectedDate: null
     };
   },
   methods: {
     async getUFValue() {
+      if (!this.selectedDate) {
+        alert('Seleccione una fecha.');
+        return;
+      }
+
+      const formattedDate = this.formatDate(this.selectedDate);
+
       try {
-        const response = await axios.get('http://localhost:3000/uf');
-        this.ufValue = response.data;
+        const response = await axios.get(`http://localhost:3000/uf/${formattedDate}`);
+        this.ufValue = response.data.serie[0];  // Obtener el primer elemento de la serie
       } catch (error) {
         console.error('Error al obtener el valor de la UF:', error);
       }
+    },
+    formatDate(date) {
+      const [year, month, day] = date.split('-');
+      return `${day}-${month}-${year}`;
+    },
+    formattedDate(dateString) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      return new Date(dateString).toLocaleDateString('es-CL', options);
     }
   }
 };
 </script>
 
 <style scoped>
-/* Estilos opcionales para tu componente*/
+/* Estilos opcionales para tu componente */
 </style>
