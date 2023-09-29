988b235f88b3edfeb88561fc5f79040a0f1bf979
felipesagredo <felipesagredogalindo@gmail.com>
2023-09-29 10:16:16 -0300

    Aplicacion Base

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const PORT = 3000;
 
 // Configuración de CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+  res.header('Access-Control-Allow-Origin', 'https://conversor-uf.netlify.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.get('/uf', async (req, res) => {
+app.get('/uf/:date', async (req, res) => {
   try {
-    const response = await axios.get('https://mindicador.cl/api');
-    const ufValue = response.data.uf.valor;
-    res.send('El valor actual de la UF es $' + ufValue + ' CLP.');
+    const date = req.params.date;
+    const response = await axios.get(`https://mindicador.cl/api/uf/${date}`);
+    res.send(response.data);
   } catch (error) {
     res.status(500).send('Error al obtener el valor de la UF');
   }
